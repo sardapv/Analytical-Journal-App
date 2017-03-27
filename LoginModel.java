6e95dc2d69e3a08a60be11d1a0683e86c901e8ea
Pranav Sarda <sardapranav1@gmail.com>
2017-03-27 08:34:19 +0530

    Login and Sign Up done with database

---
+++ LoginModel.java
@@ -0,0 +1,62 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LoginModel {
+
+
+
+    Connection connection;
+    public LoginModel(){
+        try {
+            connection = SqliteConnection.Connector();
+            if (connection == null) {
+                System.out.print("NOT CONNECTED");
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+    public boolean isDbconnected() {
+        try {
+            return !(connection.isClosed());
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+    public boolean login(String userid, String userpass) throws SQLException {
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        String query = "SELECT * FROM user WHERE username = ? AND passkey = ?";
+        try{
+            preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1,userid);
+            preparedStatement.setString(2,userpass);
+            resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                return true;
+            }
+            else {
+              // System.out.println(resultSet.next());
+                return false;
+            }
+        }
+        catch (SQLException e){
+            e.printStackTrace();
+        }
+        finally {
+            preparedStatement.close();
+            resultSet.close();
+        }
+        return false;
+
+    }
+
+}
